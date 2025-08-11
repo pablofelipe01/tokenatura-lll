d04f56702f8c80d2a2c4a7c77dbe8d0bb6855df9
Pablo Acevedo <pablofelipe@me.com>
2025-08-11 15:48:43 -0500

    Add sitemap and robots.txt generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.tokenatura.co',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/api/*'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
